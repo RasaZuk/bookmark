2cc48261fa856d1631c4368f31a88c5af5fd4d70
Rasa <rasa.zukauskaite@gmail.com>
2025-01-16 16:21:36 +0200

    primary validation

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const modalShow = document.getElementById('show-modal');
 const modalClose = document.getElementById('close-modal');
 const bookmarkForm = document.getElementById('bookmark-form');
 const websiteNameEl = document.getElementById('website-name');
-const websiteUrlEl = document.getElementById('website-erl');
+const websiteUrlEl = document.getElementById('website-url');
 const bookmarksContainer = document.getElementById('bookmarks-container');
 
 //show modal, focus on input
@@ -17,3 +17,32 @@ function showModal() {
 modalShow.addEventListener('click', showModal);
 modalClose.addEventListener('click', () => modal.classList.remove('show-modal'));
 window.addEventListener('click', (e) => (e.target === modal ? modal.classList.remove('show-modal') : false));
+
+//validate form
+function validate(nameValue, urlValue) {
+    const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+    const regex = new RegExp(expression);
+    if (urlValue.match(regex)) {
+        alert('match');
+    }
+    if (!urlValue.match(regex)) {
+        alert('Please add a valid webside address');
+        return false;
+    }
+}
+
+//handle data from form
+function storeBookmark(e) {
+    e.preventDefault();
+    const nameValue = websiteNameEl.value;
+    let urlValue = websiteUrlEl.value;
+    if (!urlValue.includes('https://') && !urlValue.includes('http://')) {
+        urlValue = `https://${urlValue}`;
+    }
+    console.log(nameValue, urlValue);
+    validate(nameValue, urlValue);
+}
+
+
+//event listener
+bookmarkForm.addEventListener('submit', storeBookmark);
